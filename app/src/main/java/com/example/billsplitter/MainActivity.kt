55df2b8dda424d5b8060b35cdd0b74ec5a593f82
IgnacioLazZam <ignacio.laz.zam@gmail.com>
2024-10-01 08:38:08 +0200

    Corrección de reactivación de slider tras ladear el móvil.

--- app/src/main/java/com/example/billsplitter/MainActivity.kt
+++ app/src/main/java/com/example/billsplitter/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.materialswitch.MaterialSwitch
 import com.google.android.material.slider.Slider
 import com.google.android.material.textfield.TextInputEditText
-import java.text.NumberFormat
+
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n", "MissingInflatedId")
@@ -33,46 +33,47 @@ class MainActivity : AppCompatActivity() {
         val switch: MaterialSwitch = findViewById(R.id.switch_inp)
 
 
-        switch.setOnClickListener(){
-            if(switch.isChecked){
-                slider.isEnabled = true;
-            } else {
-                slider.isEnabled = false
-            }
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            slider.isEnabled = isChecked
         }
+
         btnDividir.setOnClickListener {
 
-            var value1 = input1.text.toString().toFloat()
-            var value2 = input2.text.toString().toFloat()
-            var roundedTotal = value1;
+            if (input1.text.toString() != "" && input2.text.toString() !=""){
+                val value1 = input1.text.toString().toFloat()
+                val value2 = input2.text.toString().toFloat()
+                var roundedTotal = value1;
 
-            if (switch.isChecked){
+                if (switch.isChecked){
 
-                roundedTotal = when (slider.value){
-                    1.0f -> value1 * 1.05f
-                    2.0f -> value1 * 1.10f
-                    3.0f -> value1 * 1.15f
-                    4.0f -> value1 * 1.20f
-                    5.0f -> value1 * 1.25f
-                    else -> value1 * 1.0f
+                    roundedTotal = when (slider.value){
+                        1.0f -> value1 * 1.05f
+                        2.0f -> value1 * 1.10f
+                        3.0f -> value1 * 1.15f
+                        4.0f -> value1 * 1.20f
+                        5.0f -> value1 * 1.25f
+                        else -> value1 * 1.0f
+                    }
                 }
-            }
 
 
-            total.text = "Cantidad total: $roundedTotal€";
+                total.text = "Cantidad total: $roundedTotal€";
 
-            if (value2 != 0f) {
-                var result = 0.0f;
-                if (!switch.isChecked){
-                    result = value1 / value2
+                if (value2 != 0f) {
+                    var result = 0.0f;
+                    if (!switch.isChecked){
+                        result = value1 / value2
+                    } else {
+                        result = roundedTotal / value2
+                    }
+
+                    resultText.text = "Cada uno: $result€"
                 } else {
-                    result = roundedTotal / value2
+                    resultText.text = "Indique uno o más participantes."
                 }
-
-                resultText.text = "Cada uno: $result€"
-            } else {
-                resultText.text = "Indique uno o más participantes."
             }
+
+
         }
     }
 }
